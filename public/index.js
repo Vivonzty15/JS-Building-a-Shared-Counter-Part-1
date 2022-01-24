587eec737cb0101f85ff6b23c8edc798c2936fc7
Braxton Allen <braxtonallen99@yahoo.com>
2022-01-23 22:30:02 -0600

    fetched data from local server

--- public/index.js
+++ public/index.js
@@ -1,13 +1,26 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response =  await fetch('http://127.0.0.1:9001/counter')
+    let jsonObject =  await response.json()
+    console.log(jsonObject)
+    countValue = jsonObject.value
 
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+
+        // fetch('http://localhost/counter', {
+        //     method: 'Patch',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //         'value': 'countValue'
+        //     })
+        // })
     }
 
     function decrement(){
